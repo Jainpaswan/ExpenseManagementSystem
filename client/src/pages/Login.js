287683bdfae371e0ab70b4febdb497be85a32079
Jain <jainkumar1050@gmail.com>
2025-03-05 21:45:06 +0530

    go

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import { Form ,Input,message} from "antd";
 import { Link,useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -8,40 +8,50 @@ const Login = () => {
     const navigate=useNavigate()
     const [loading,setLoading]=useState(false);
      //form submit
-     const submitHandler= async (values)=>{
+     const submitHandler = async (values) => {
         try {
-            setLoading(true)
-          const {data}=  await axios.post('/users/login',values)  
-          setLoading(false)
-          message.success('login success')
-          localStorage.setItem('user',JSON.stringify({...data, password:''}))
-          navigate('/')
-            
+          setLoading(true);
+          const { data } = await axios.post("/users/login", values);
+
+          setLoading(false);
+          message.success("login success");
+          localStorage.setItem(
+            "user",
+            JSON.stringify({ ...data.user, password: "" })
+          );
+          navigate("/");
         } catch (error) {
-            setLoading(false)
-            message.error('Something went wrong')
-            
+          setLoading(false);
+          message.error("something went wrong");
         }
-        
-    }
+      };
+
+      useEffect(() => {
+        if (localStorage.getItem("user")) {
+          navigate("/");
+        }
+      }, [navigate]);
+
+
   return (
     <>
-     <div className="register-page">
-        {loading && <Spinner/>}
-            <Form layout='vertical' onFinish={submitHandler} >
-                <h1>Login Form</h1>
-                <Form.Item label="Email" name="email">
-                    <Input type='email'/>
-                </Form.Item>
-                <Form.Item label="Password" name="password">
-                    <Input type="password"/>
-                </Form.Item>
-                <div className="d-flex justify-content-between">
-                    <Link to="/register">Not a user? Click to Register</Link>
-                    <button className='btn btn-primary'>Login</button>
-                </div>
-            </Form>
-        </div> 
+     <div className="register-page ">
+        {loading && <Spinner />}
+        <Form layout="vertical" onFinish={submitHandler}>
+          <h1>Login Form</h1>
+
+          <Form.Item label="Email" name="email">
+            <Input type="email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" />
+          </Form.Item>
+          <div className="d-flex justify-content-between">
+            <Link to="/register">Not a user ? Cleck Here to regsiter</Link>
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </Form>
+      </div>
     </>
   )
 }
