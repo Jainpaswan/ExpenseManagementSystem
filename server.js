91bb9150cf68ca38e99b51e4b7c9301019997456
Jain <jainkumar1050@gmail.com>
2026-03-12 17:23:58 +0530

    da

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan=require('morgan')
 const dotenv=require('dotenv')
 const colors=require('colors')
 const connectDb=require('./config/connectDb')
+// const path=require('path');
 
 dotenv.config();
 
@@ -17,6 +18,11 @@ app.use(cors())
 
 app.use('/api/v1/users',require("./routes/userRoute"));
 app.use('/api/v1/transections',require("./routes/transectionRoutes"));
+
+// app.use(express.static(path.join(__dirname,'./client/build')))
+// app.get('*',function(req,res){
+//     res.sendFile(path.join(__dirname,'./client/build/index.html'));
+// });
 
 const PORT=8080 || process.env.PORT
 
